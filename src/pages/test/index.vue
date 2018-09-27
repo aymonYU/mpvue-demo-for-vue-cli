3cd6c90893a31e4ec4ba47df2be865e26ea25188
aymonyu(余文明) <aymonyu@tencent.com>
2018-09-27 14:37:46 +0800

    commit init

---
+++ src/pages/test/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h2 class="title">333</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.title {
+  padding-top: 200px;
+  text-align: center;
+}
+</style>
